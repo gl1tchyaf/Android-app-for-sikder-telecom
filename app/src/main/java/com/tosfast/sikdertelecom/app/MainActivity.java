6f7d3865a93d61515a36a25900bd4407b00a44f3
gl1tchy_af <fahad.com66.fk@gmail.com>
2022-01-21 22:56:34 +0600

    Initial commit

---
+++ app/src/main/java/com/tosfast/sikdertelecom/app/MainActivity.java
@@ -0,0 +1,24 @@
+package com.tosfast.sikdertelecom.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int SPLASH_TIME_OUT= 2000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(() -> {
+            Intent loginIntent = new Intent(MainActivity.this, webview.class);
+            startActivity(loginIntent);
+            finish();
+        },SPLASH_TIME_OUT);
+
+
+    }
+}
