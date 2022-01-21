b161e46165589119f1ff92fa65ef397dce3dff21
gl1tchy_af <fahad.com66.fk@gmail.com>
2022-01-22 01:18:52 +0600

    drop down option added

--- app/src/main/java/com/tosfast/sikdertelecom/app/webview.java
+++ app/src/main/java/com/tosfast/sikdertelecom/app/webview.java
@@ -4,15 +4,19 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class webview extends AppCompatActivity {
+public class webview extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
     private WebView webView;
 
     @SuppressLint("SetJavaScriptEnabled")
@@ -31,8 +35,47 @@ public class webview extends AppCompatActivity {
         Button home = findViewById(R.id.home);
         Button contact = findViewById(R.id.Contact);
         Button shop = findViewById(R.id.Shop);
-        Button login = findViewById(R.id.Login);
 
+        ImageButton b=findViewById(R.id.menu);
+        b.setOnClickListener(this::showPopup);
+
+
+    }
+
+    public void showPopup(View v) {
+        PopupMenu popup = new PopupMenu(this, v);
+        popup.setOnMenuItemClickListener((PopupMenu.OnMenuItemClickListener) this);
+        //MenuInflater inflater = popup.getMenuInflater();
+        popup.inflate(R.menu.dropdown_menu);
+        popup.show();
+
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        if(item.getItemId()==R.id.login){
+            System.out.println("Login");
+            return true;
+        }
+        if(item.getItemId()==R.id.register){
+            System.out.println("Register");
+            return true;
+        }
+        if(item.getItemId()==R.id.wholesaller){
+            System.out.println("wholesaller");
+            return true;
+        }
+        if(item.getItemId()==R.id.about){
+            System.out.println("about");
+            return true;
+        }
+        if(item.getItemId()==R.id.exit){
+            System.out.println("exit");
+            return true;
+        }
+        else{
+            return false;
+        }
 
     }
 
